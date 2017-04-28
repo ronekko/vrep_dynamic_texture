2e5ec1b92f8486233f86618bdebdabebe68805c3
sakurai <sakurai@ais.ics.ritsumei.ac.jp>
2017-04-28 22:11:05 +0900

    Write Vrep class

--- main.py
+++ main.py
@@ -13,52 +13,76 @@ import six
 import vrep
 
 
-if __name__ == '__main__':
-    try:
-        client_id
-    except NameError:
-        client_id = -1
-    e = vrep.simxStopSimulation(client_id, vrep.simx_opmode_oneshot_wait)
-    vrep.simxFinish(-1)
-    client_id = vrep.simxStart(six.b('127.0.0.1'), 19998, True, True, 5000, 5)
+class Vrep(object):
+    def __init__(self, address='127.0.0.1', port=19998,
+                 wait_until_connected=True,
+                 do_not_reconnect_once_disconnected=True, time_out_in_ms=5000,
+                 comm_thread_cycle_in_ms=5):
+        '''See http://www.coppeliarobotics.com/helpFiles/en/remoteApiFunctionsPython.htm#simxStart
+        '''
 
-    assert client_id != -1, 'Failed to connect to the V-rep server'
+        vrep.simxStopSimulation(-1, vrep.simx_opmode_oneshot_wait)
+        vrep.simxFinish(-1)
+        client_id = vrep.simxStart(six.b(address), port, wait_until_connected,
+                                   do_not_reconnect_once_disconnected,
+                                   time_out_in_ms, comm_thread_cycle_in_ms)
+        assert client_id != -1, 'Failed to connect to the V-rep server'
 
-    e = vrep.simxStartSimulation(client_id, vrep.simx_opmode_oneshot_wait)
-    # Handles of body and wheels
-    e, vision = vrep.simxGetObjectHandle(client_id, six.b('Vision_sensor'),
-                                         vrep.simx_opmode_oneshot_wait)
+        vrep.simxStartSimulation(client_id, vrep.simx_opmode_oneshot_wait)
+        self.client_id = client_id
+        self.handles = {}
 
-    for i in range(10000):
-        print(i)
-        image_width, image_height = 128, 128
-#        image = np.random.randint(0, 255, size=(128, 128, 3), dtype=np.uint8)
-        theta = np.pi * float(i) / 60
-        r = (np.sin(theta / 10) + 1) / 2.0
-        x = r * np.cos(theta)
-        y = r * np.sin(theta)
-        fig = Figure(figsize=(image_height / 100.0, image_width / 100.0))
-        ax = fig.gca()
-        ax.plot(x, y, '.', markersize=20)
-        ax.set_ylim(-1, 1)
-        ax.set_xlim(-1, 1)
-        ax.set_aspect('equal', adjustable='box')
-        canvas = FigureCanvasAgg(fig)
-        canvas.draw()
-        image = np.fromstring(canvas.tostring_rgb(), dtype='uint8')
-        image = image.reshape((image_height, image_width, 3))
+    def register_handle(self, handle_name):
+        _, handle = vrep.simxGetObjectHandle(
+            self.client_id, six.b(handle_name), vrep.simx_opmode_oneshot_wait)
+        self.handles[handle_name] = handle
 
+    def set_image(self, handle_name, image):
         if image.ndim == 3:
             is_color = 0
         elif image.ndim == 2:
             is_color = 1
         else:
-            raise ValueError('"image" must be np.ndarray of (H, W, C)-shaped'
+            raise ValueError('`image` must be np.ndarray of (H, W, C)-shaped'
                              '3D array (i.e. color) or (H, W)-shaped 2D array'
                              '(i.e. gray scale)')
         serial_image = image.ravel()
-        e = vrep.simxSetVisionSensorImage(client_id, vision, serial_image,
-                                          is_color, vrep.simx_opmode_oneshot)
-        vrep.simxSynchronousTrigger(client_id)
+        vrep.simxSetVisionSensorImage(
+            self.client_id, self.handles[handle_name], serial_image,
+            is_color, vrep.simx_opmode_oneshot)
 
-    e = vrep.simxStopSimulation(client_id, vrep.simx_opmode_oneshot_wait)
+    def send(self):
+        vrep.simxSynchronousTrigger(self.client_id)
+
+
+if __name__ == '__main__':
+    simulator = Vrep()
+    vision1 = 'Vision_sensor'
+    simulator.register_handle(vision1)
+
+    image_width, image_height = 128, 128
+
+    for i in range(10000):
+        # example 1: random noise
+        image = np.random.randint(
+            0, 255, size=(image_width, image_height, 3), dtype=np.uint8)
+
+#        # example 2: moving ball
+#        theta = np.pi * float(i) / 60
+#        r = (np.sin(theta / 4) + 1) / 2.0
+#        x = r * np.cos(theta)
+#        y = r * np.sin(theta)
+#        fig = Figure(figsize=(image_height / 100.0, image_width / 100.0))
+#        ax = fig.gca()
+#        ax.plot(x, y, '.', markersize=20)
+#        ax.set_ylim(-1, 1)
+#        ax.set_xlim(-1, 1)
+#        ax.set_aspect('equal', adjustable='box')
+#        canvas = FigureCanvasAgg(fig)
+#        canvas.draw()
+#        image = np.fromstring(canvas.tostring_rgb(), dtype='uint8')
+#        image = image.reshape((image_height, image_width, 3))
+
+        simulator.set_image(vision1, image)
+        simulator.send()
+        print(i)
