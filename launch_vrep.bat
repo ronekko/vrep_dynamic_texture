66af19221d3474aa53847dde0843b2d93a69346f
ronekko <ronekko@gmail.com>
2017-04-29 02:02:23 +0900

    Check install directory of vrep.exe

--- launch_vrep.bat
+++ launch_vrep.bat
@@ -1,3 +1,16 @@
+set dirname="C:\Program Files\V-REP3\V-REP_PRO_EDU\"
+if exist %dirname%vrep.exe (goto LAUNCH) else goto CHECK_X86
+
+:CHECK_X86
+set dirname="C:\Program Files\V-REP3 (x86)\V-REP_PRO_EDU\"
+if exist %dirname%vrep.exe (goto LAUNCH) else goto NOT_FOUND
+
+:LAUNCH
 set "CURRENT_DIR=%cd%"
-cd "C:\Program Files (x86)\V-REP3\V-REP_PRO_EDU"
+cd %dirname%
 vrep -gREMOTEAPISERVERSERVICE_19998_FALSE_TRUE %CURRENT_DIR%\simple_box.ttt
+goto END
+
+:NOT_FOUND
+echo "ERROR: vrep.exe is not found."
+:END
