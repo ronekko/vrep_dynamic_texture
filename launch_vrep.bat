30b8243ed236e0ebe0f23cd028fd953c2e1cfd81
sakurai <sakurai@ais.ics.ritsumei.ac.jp>
2017-05-02 18:46:34 +0900

    Rename scene file

--- launch_vrep.bat
+++ launch_vrep.bat
@@ -8,7 +8,7 @@ if exist %dirname%vrep.exe (goto LAUNCH) else goto NOT_FOUND
 :LAUNCH
 set "CURRENT_DIR=%cd%"
 cd %dirname%
-vrep -gREMOTEAPISERVERSERVICE_19998_FALSE_TRUE %CURRENT_DIR%\simple_box.ttt
+vrep -gREMOTEAPISERVERSERVICE_19998_FALSE_TRUE %CURRENT_DIR%\simple.ttt
 goto END
 
 :NOT_FOUND
